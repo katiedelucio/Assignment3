827ad7cb5e27385fc5aac4d909546c7c88ac5bde
katiedelucio <125385457+katiedelucio@users.noreply.github.com>
2023-03-07 20:48:40 -0500

    Add files via upload

--- Bank.java
+++ Bank.java
@@ -16,24 +16,23 @@ public class Bank {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		MyAccounts kd = new MyAccounts();
-		kd.createAccounts();
-		kd.displayAccountInfo();
-		
-	}
-}
+		// create ArrayList to store bank accounts
+		ArrayList<Account> bankaccounts = new ArrayList<>();
 
-class MyAccounts {
-	// create ArrayList to store bank accounts
-	ArrayList<Account> bankaccounts = new ArrayList<>();
-
-	public void createAccounts() {
 		// CheckingAccount needs int accountNumber, double balance, String branch,
 		// String dateOpened,String checkStyle, double minimumBalance
 		CheckingAccount acct1 = new CheckingAccount(1, 100, "Chicago", "1/31/22", "Fancy style", 50.00);
 		bankaccounts.add(acct1);
+		// create customers-Commercial
+		// String name, String address, String phone, Account account, double
+		// creditRating,
+		// String contactPerson, String contactPersonPhone
+		CommercialCustomer jan = new CommercialCustomer("Jan Doe", "11 Main Street", "876-213-3333", acct1, 300,
+				"Jeff Doe", "876-213-3333");
+		displayInfo(jan);
 		acct1.makeDeposit(100);
-		
+		displayInfo(jan);
+		// create 2nd checking account and add to arraylist
 		CheckingAccount acct2 = new CheckingAccount(2, 200, "Indianapolis", "10/30/19", "Basic style", 50.00);
 		bankaccounts.add(acct2);
 
@@ -41,38 +40,21 @@ class MyAccounts {
 		// dateOpened, double interestRate
 		SavingsAccount acct3 = new SavingsAccount(3, 100, "Denver", "1/28/18", 10.0);
 		bankaccounts.add(acct3);
-		
+//create personal customer
+		// String name, String address, String phone, Account account, String homePhone,
+		// String workPhone)
+		PersonalCustomer rod = new PersonalCustomer("Rod Jones", "1100 My Way", "555-222-4444", acct3, "555-222-4444",
+				"555-123-4567");
+		displayInfo(rod);
 		acct3.makeDeposit(100);
-		System.out.println(bankaccounts.toString());
-		//System.out.println("The interest earned is " + acct3.calculateInterest() + " per month.");
-		
+		acct3.calculateInterest();
+		System.out.println(
+				"The new balance is " + acct3.getBalance() + " and monthly interest is " + acct3.calculateInterest());
 
 	}
-	//find account so it can be displayed in displayAccountInfo
-	public Account findAccount(int accountNumber) {
-		for (Account a : bankaccounts) {
-			if (a.getAccountNumber() == accountNumber);
-			return a;
-		}
-		return null;
+
+	public static void displayInfo(Customer x) {
+		System.out.println("\n" + x.toString());
+
 	}
-
-	// display account info given the account number
-	public void displayAccountInfo() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("\nEnter the account number of the account that you want to display: ");
-		int acctn = input.nextInt();
-		Account a = findAccount(acctn);
-		if (a == null) {
-			System.out.println("\nAccount not found.");
-		} else {
-			System.out.println();
-			System.out.println("Details for the account with the number " + acctn + ": ");
-			System.out.println(a);
-			System.out.println();
-		}
-	}	
-	}
-
-
-
+}
